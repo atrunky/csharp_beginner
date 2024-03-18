4abe42c6cf6c60574f47d260fb9c8d31f64b9dbd
atrunky <atrunky@gmail.com>
2024-03-18 11:41:06 -0500

    Update data

--- HellowWorld/Program.cs
+++ HellowWorld/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+//Console.WriteLine("Hello, World!");
+//Console.ReadLine();
+namespace HelloWorld{
+    class Program {
+
+        static void Main(){
+            string Name="Pepito";
+            Console.WriteLine($"Hello world {Name}");
+            Console.WriteLine();
+        }
+    }
+}

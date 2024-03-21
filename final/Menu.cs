da7f4bbe7752e13ae243a7438024daf5b050c8da
atrunky <atrunky@gmail.com>
2024-03-20 21:49:02 -0500

    Actualizacion nuevo branch07 = FINAL

---
+++ final/Menu.cs
@@ -0,0 +1,26 @@
+namespace itm.csharp.basic
+{
+    public static class Menu
+    {
+        public static void MostrarMenu()
+        {
+            Console.WriteLine("1. INDICATES IF A NUMBER IS POSITIVE, NEGATIVE OR ZERO. ");
+            Console.WriteLine("2. BASIC OPERATIONS BETWEEN TWO NUMBERS. ");
+            Console.WriteLine("3. SQUARE OF A NUMBER. ");
+            Console.WriteLine("4. AREA OF A CIRCLE. ");
+            Console.WriteLine("5. WEEKDAY. ");
+            Console.WriteLine("6. TAX PAYABLE. ");
+            Console.WriteLine("7. DIVISION OF 2 NUMBERS ");
+            Console.WriteLine("8. CALCULATE AND SHOW THE SUM OF ODD NUMBERS BETWEEN 1 AND 100. ");
+            Console.WriteLine("9. ADDING FRACTIONS. ");
+            Console.WriteLine("10. REQUEST WORD FROM THE USER AND SHOW IT INVERTED. ");
+            Console.WriteLine("11. ASKS THE USER FOR 3 NUMBERS AND SHOWS THEIR AVERAGE. ");
+            Console.WriteLine("12. ASK THE USER FOR 5 NUMBERS AND SHOW THE LARGEST ONE. ");
+            Console.WriteLine("13. ENTER A WORD, IF OR NOT IT IS PALINDROME. ");
+            Console.WriteLine("14. THE USER ENTER NUMBER AND IT IS SHOWN IF IT IS EVEN OR ODD ");
+            Console.WriteLine("15. ENTER A NUMBER AND VALIDATE IF IT IS BETWEEN THE RANGE OF 0 TO 10. ");
+            Console.WriteLine("Enter the number for select options:  ");
+        }
+    }
+
+}
